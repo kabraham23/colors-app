f0a3b62d2350bfe3d7399f1d77f11430ef270420
Katie A <katie.t.abraham@gmail.com>
2023-02-22 17:29:49 -0600

    adding files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Palette from "./Palette";
+import seedColors from './seedColors'
+
+function App() {
+  return (
+    <div className="App">
+      <Palette {...seedColors[5]} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Palette.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div className='Palette'>
+        <div className='Pallette=colors'></div>
+      </div>
+    )
+  }
+}
