d21fdd75230512ec1741d3fd1e99f735e7b09f95
Katie A <katie.t.abraham@gmail.com>
2023-02-23 13:22:38 -0600

    adding styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import Palette from "./Palette";
+import Palette from "./Components/Palette";
 import seedColors from './seedColors'
 
 function App() {
   return (
     <div className="App">
-      <Palette {...seedColors[5]} />
+      <Palette {...seedColors[4]} />
     </div>
   );
 }
