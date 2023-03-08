308faf1d2dbc67c813da52df5cdd39197c69999c
Katie A <katie.t.abraham@gmail.com>
2023-03-08 13:55:20 -0600

    updating routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
+import { Route, Routes } from 'react-router-dom';
 import Palette from "./Components/Palette";
+import PaletteList from './Components/PaletteList';
 import seedColors from './seedColors';
 import { generatePalette } from "./colorHelpers";
 
-function App() {
-  console.log((generatePalette(seedColors[4])))
-  return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
-  );
+class App extends Component {
+  findPalette(id){
+    return seedColors.find(function(palette){
+      return palette.id === id;
+    })
+  }
+  render() {
+    return (
+      <>
+      <Routes>
+        <Route exact path="/" element={<PaletteList />} />
+        <Route 
+        exact 
+        path="/palette/:id" 
+        render={routeProps => (
+          <Palette palette={generatePalette(
+            this.findPalette(routeProps.match.params.id)
+            )} />
+        )} />
+      </Routes>
+      </>
+    );
+  }
 }
 
 export default App;
