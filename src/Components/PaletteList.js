42a7ad540e420b71ed2e14856d83a5b10354c455
Katie A <katie.t.abraham@gmail.com>
2023-04-14 10:29:35 -0500

    installing material ui styles

--- src/Components/PaletteList.js
+++ src/Components/PaletteList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MiniPalette from './MiniPalette';
 import { Link } from 'react-router-dom';
 
 export default class PaletteList extends Component {
@@ -6,6 +7,7 @@ export default class PaletteList extends Component {
     const { palettes } = this.props;
     return (
       <div className='PaletteList'>
+        <MiniPalette />
         <h1>List</h1>
         {palettes.map((palette, index) => (
             <p key={index}>
