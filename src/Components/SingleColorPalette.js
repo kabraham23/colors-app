54ef0acb92cd9429574bd6461f23aa485de3d96d
Katie A <katie.t.abraham@gmail.com>
2023-05-05 09:34:15 -0500

    style single color palette

--- src/Components/SingleColorPalette.js
+++ src/Components/SingleColorPalette.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import ColorBox from './ColorBox';
 import NavBar from './NavBar';
 import PaletteFooter from './PaletteFooter';
@@ -26,7 +27,7 @@ class SingleColorPalette extends Component {
     }
     render() {
         const {format} = this.state;
-        const { paletteName, emoji } = this.props.palette;
+        const { paletteName, emoji, id } = this.props.palette;
         const colorBoxes = this._shades.map(color => (
             <ColorBox 
                 key={color.name} 
@@ -36,9 +37,14 @@ class SingleColorPalette extends Component {
             />
         ));
         return (
-            <div className='Palette'>
+            <div className='SingleColorPalette Palette'>
                 <NavBar handleChange={this.changeFormat} showingAllColors={false} />
-                <div className='Palette-colors'>{colorBoxes}</div>
+                <div className='Palette-colors'>
+                    {colorBoxes}
+                    <div className="go-back ColorBox">
+                        <Link to={`/palette/${id}`} className='back-button'>Go Back</Link>
+                    </div>
+                </div>
                 <PaletteFooter paletteName={paletteName} emoji={emoji} />
             </div>
         )
